df5f60860d15fe550200e02a900710134cf2b4c3
Mark G <markgoberdhan@gmail.com>
2019-10-17 10:18:57 -0400

    Update control_structures.go

--- control_structures.go
+++ control_structures.go
@@ -2,9 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello world")
-
+func forControl() {
 	f := true
 	flag := &f // were pointing to the address allocated to f in mem. Using the referencing operator
 
@@ -19,7 +17,9 @@ func main() {
 	for i := 0; i < 3; i++ {
 		fmt.Println(i)
 	}
+}
 
+func arr() {
 	// create an arr of type string
 	arr := []string{"Mark", "luke", "enginer"}
 
@@ -28,7 +28,9 @@ func main() {
 		fmt.Println("indx is ", i)
 		fmt.Println("value is ",value)
 	}
+}
 
+func mapIt() {
 	// map with key of str and value of interface__or__anything!
 	myMap := make(map[string]interface{})
 	myMap["name"] = "mrk"
@@ -37,4 +39,37 @@ func main() {
 	for k,v := range myMap {
 		fmt.Printf(" Key: %s and value: %v", k, v)
 	}
+}
+
+func switchControl() {
+	flag := true
+	for i := 1; i < 10 ; i++ {
+		if i % 2 == 0 {
+			flag=false
+			break
+		} else if i == 1 {
+			continue
+		}
+	}
+	fmt.Println(flag)
+
+	day := "Fri"
+	switch day {
+	case "Fri":
+		fmt.Println("its fiday friday")
+	case "mon", "tue", "wed":
+		fmt.Println("its midweek")
+	default:
+		fmt.Println("default")
+	}
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	// forControl()
+	// arr()
+	// mapIt()
+	// switchControl()
+	switchControl()
 }
